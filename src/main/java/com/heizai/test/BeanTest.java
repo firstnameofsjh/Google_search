3a7fe5b41e236a8bb2e26de090cd3244141c4895
Administrator <Administrator@KVM8SCFD7UQ24LE.lan>
2020-02-10 10:09:23 +0800

    myTest

--- src/main/java/com/heizai/test/BeanTest.java
+++
@@ -1,7 +0,0 @@
-package com.heizai.test;
-
-public class BeanTest {
-	public static void main(String[] args) {
-		System.out.println("qqqq000000000q5555q");
-	}
-}
---
+++ src/main/java/com/heizai/test/Test.java
@@ -0,0 +1,8 @@
+package main.java.com.heizai.test;
+
+public class Test {
+	public void getTest() {
+		String format = String.format("", "");
+		System.out.println(format);
+	}
+}
